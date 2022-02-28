6c6a3802e0757f2732c57b312a31e039905ff438
Mi-kio <sakshihere34@gmail.com>
2022-02-28 18:12:34 +0530

    made corrections, color problem still there

--- script.js
+++ script.js
@@ -2,13 +2,33 @@ document.addEventListener("DOMContentLoaded",()=>{
 
     createSquares();
 
-    const guessedWords = [[]]
+    const guessedWords = [[]];
     let availableSpace = 1; 
     let word = "dairy";
     let guessedWordsCount = 0;
-const keys=document.querySelectorAll('.keyboard-row button');
+    const keys=document.querySelectorAll('.keyboard-row button');
 
-
+    function getNewWord() {
+        fetch(
+          `https://wordsapiv1.p.rapidapi.com/words/?random=true&lettersMin=5&lettersMax=5`,
+          {
+            method: "GET",
+            headers: {
+              "x-rapidapi-host": "wordsapiv1.p.rapidapi.com",
+              "x-rapidapi-key": "<YOUR_KEY_GOES_HERE>",
+            },
+          }
+        )
+          .then((response) => {
+            return response.json();
+          })
+          .then((res) => {
+            word = res.word;
+          })
+          .catch((err) => {
+            console.error(err);
+          });
+      }
 
 function getCurrentWordArr(){
     const numberOfGuessedWords = guessedWords.length
